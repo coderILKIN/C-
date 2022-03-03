9e6cee358c6b7c1b36a670319037054b20030f0a
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-04 02:14:03 +0400

    Console penceresinde 2 eded daxil edin. Daxil olunan 2 ededden hansinin boyuk oldugu ekrana yazilsin. Eger beraberdirse ekrana beraberdir yazilsin

---
+++ 2 ededden hansinin boyuk oldugu/2 ededden hansinin boyuk oldugu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2_ededden_hansinin_boyuk_oldugu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (number > num)
+            {
+                Console.WriteLine("number boyukdur");
+
+            }
+
+            else if (number < num)
+            {
+                Console.WriteLine("num boyukdur");
+
+            }
+
+            else if (number == num)
+            {
+                Console.WriteLine("beraberdir");
+            }
+        }
+    }
+}
