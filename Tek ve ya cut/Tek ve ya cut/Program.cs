6ef1764f891c6eaddfa3ba09bd2e475539198681
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-04 02:08:21 +0400

    Console penceresinde 1 eded daxil edin. Daxil olunan ededin tek ve ya cut oldugunu ekrana yazdirin.

---
+++ Tek ve ya cut/Tek ve ya cut/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tek_ve_ya_cut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num%2 == 1)
+            {
+                Console.WriteLine("Eded tekdir");
+            }
+
+            else
+            {
+                Console.WriteLine("Eded cutdur");
+            }
+
+
+        }
+    }
+}
